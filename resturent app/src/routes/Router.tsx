fa65da350564a4290b0c7a4c9202f2e1b67a6d6f
Omnifics Technologies <omnificstechnologies@Omnificss-MacBook-Pro.local>
2025-03-25 15:19:03 +0500

    feature: feedback

--- resturent app/src/routes/Router.tsx
+++ resturent app/src/routes/Router.tsx
@@ -1,14 +1,13 @@
 import LoginForm from "@/components/SignIn-form";
 import { RegisterForm } from "../components/Register-form";
 import { RestaurantMenu } from "@/components/components/restaurant-menu";
-import { BrowserRouter as Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import RestaurentSelection from "@/components/RestaurentSelection";
   import Customer from "@/components/Customer";
 
 export default function AppRouter() {
 
   return (
-    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/dashboard" element={<RestaurantMenu />} />
@@ -16,6 +15,5 @@ export default function AppRouter() {
         <Route path="/customer" element={<Customer />} />
         <Route path="/restaurent-selection" element={<RestaurentSelection />} />
       </Routes>
-    </BrowserRouter>
   );
 }
